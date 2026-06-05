9d092c60d2d07a538d10845ac2885d0e5e00f8fb
Anup C <anupchandratre@gmail.com>
2026-06-05 09:29:47 +0000

    rename

---
+++ src/main/java/com/anup/ai/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package com.anup.ai.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+}
